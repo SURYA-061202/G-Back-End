e1c0c7df82b6abe5b586968f4a37b8e01abfc423
SURYA-061202 <132092219+SURYA-061202@users.noreply.github.com>
2024-01-26 18:47:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require("cors")
+const EmployeeModel = require('./models/Employee')
+
+const app = express()
+app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // or specify the allowed origin(s)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+mongoose.connect(`${process.env.MONGODB_URL}`).then(() =>{
+    console.log("mongodb connected");
+}).
+catch(() =>{
+    console.log("failed");
+});
+app.post('/login', (req, res) =>{
+    const {email, password} = req.body;
+    EmployeeModel.findOne({email: email})
+    .then(user =>{
+        if(user){
+            if(user.password === password){
+                res.json("Success")
+            }
+            else{
+                res.json("The password is incorrect")
+            }
+        }
+        else{
+            res.json("No record existed")
+        }
+    })
+})
+app.post('/register', (req, res) =>{
+    EmployeeModel.create(req.body)
+    .then(employees => res.json(employees))
+    .catch(err => res.json(err))
+
+
+})
+//mongodb+srv://<username>:<password>@cluster0.2zzpwtq.mongodb.net/
+app.listen(3001, () => {
+    console.log("Server is running")
+})
