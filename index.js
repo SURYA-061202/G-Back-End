ce3a4ebcd25ccc35fe2b96bec7066dce3724c187
SURYA-061202 <132092219+SURYA-061202@users.noreply.github.com>
2024-01-26 21:33:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const EmployeeModel = require('./models/Employee')
 
 const app = express()
 app.use(express.json())
+app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // or specify the allowed origin(s)
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 mongoose.connect(`${process.env.MONGODB_URL}`).then(() =>{
     console.log("mongodb connected");
 }).
